7c7e128ec398ac01830a8a7b8e5f8ddce23103dc
dpritam02 <daspritam161202@gmail.com>
2024-11-17 12:27:33 +0530

    ini 2

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -30,10 +30,6 @@ class Bike{
         this.color = C;
     }*/
 
-    public void display(){
-        System.out.println("Gear: "+gear+ " ,Stroke: "+stroke+ " ,Color: "+color);
-    }
-
     public String toString(){
         return ("Gear: " + gear + " ,Stroke: " + stroke + " ,Color: " + color);
     }
@@ -59,21 +55,81 @@ class duke extends Bike{
     }
 }
 
+
+//reason for override--
+/*@Override
+public String toString() {
+    // Using getters to access private fields from the parent class
+    return "Gear: " + getGear() + " ,Stroke: " + getStroke() + " ,Color: " + getColor() + " ,Engine: " + engine;
+}
+Why Override: This method overrides toString() to return a custom string that includes the details of the Duke object, such as gear, stroke, color, and engine.
+Usage of Getters: Since the fields in the parent class Bike are private, the method uses getters (getGear(), getStroke(), getColor()) to access these values. This ensures that the method respects encapsulation while still producing a formatted string of the object's state.
+@Override Annotation: This annotation indicates that the method is overridden from a superclass, providing clarity and helping catch errors if the method signature does not match the method in the superclass.
+If you create and print a Duke object like this:
+
+java
+Copy code
+Duke d1 = new Duke(4, 2, "Orange", 6);
+System.out.println(d1.toString());
+The overridden toString() method will output:
+
+less
+Copy code
+Gear: 4 ,Stroke: 2 ,Color: Orange ,Engine: 6
+This is much more informative than the default toString() method from the Object class, which would print something like Duke@1a2b3c4d.
+*/
+
 /*class pulser { //here the gear varaible is not accessable to this pulser class due to protected
     public void display(){
         System.out.println(gear);
     }
 }*/
 
+
+
 public class Inheritance {
     public static void main(String[] args) {
+
+        // inheritance understanding
         //parent class result
-        Bike b1 = new Bike(4,2,"Blue");
+        /*Bike b1 = new Bike(4,2,"Blue");
         System.out.println("Parent class");
-        b1.display();
+        System.out.println(b1.toString());
         //child class result
         duke d1 = new duke(4,2,"Orange", 6);
         System.out.println("Child class");
         System.out.println(d1.toString());
+         */
+
+        //types of inheritance
+        //1. single -> lion - animal
+        lion l1 = new lion();
+        l1.eat();
+
+        //2. multilevel -> cub - lion - animal
+        cub c1 = new cub();
+        c1.eat();
+        c1.sound();
+
+        //3. Hybrid
+
     }
 }
+
+//types of inheritance---
+class Animal{
+    int tier;
+    public void eat(){
+        System.out.println("Carnivorous");
+    }
+}
+class lion extends Animal{
+    int cry;
+    public void sound(){
+        System.out.println("Roar");
+    }
+}
+class cub extends lion{
+}
+
+
